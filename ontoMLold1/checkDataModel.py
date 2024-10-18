1a7aec1a92d945f9dc010ef1d38693484a16ffa9
Ubuntu <ubuntu@remote-host-1.osc.eonerc.rwth-aachen.de>
2024-10-18 23:51:34 +0000

    Initial commit

--- ontoMLold1/checkDataModel.py
+++
@@ -1,5 +0,0 @@
-import urllib.request, urllib.response, urllib.parse, urllib.error
-
-urlHandler = urllib.request.urlopen('https://sargon-n5geh.netlify.app/ontologies/Sargon.ttl')
-for line in urlHandler:
-    print(line.decode().strip())
--- ontoMLold2/run.py
+++
@@ -1,8 +0,0 @@
-from ontoML import create_app
-from ontoML.ontoai_builder import create_ontology, onto_to_graph, save_ontology, visualize_graph
-from ontoML.ontoai_validator import run_reasoning, validate_ontology_with_shacl
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
